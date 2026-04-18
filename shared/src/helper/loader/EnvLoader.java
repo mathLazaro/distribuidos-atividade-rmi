17c79315a81690d7beb0db138fe06bc64d288c2d
Matheus Lázaro de Lima <matheus.lazaro2002@gmail.com>
2026-04-18 18:40:44 -0300

    doc: documentação do EnvLoader

--- shared/src/helper/loader/EnvLoader.java
+++ shared/src/helper/loader/EnvLoader.java
@@ -7,10 +7,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Classe utilitária para carregar variáveis de ambiente a partir de um arquivo
+ * de ambiente (.env).
+ */
 public class EnvLoader {
 
     private static final Map<String, String> env = new HashMap<>();
 
+    /**
+     * Carrega as variáveis de ambiente de um arquivo de ambiente (.env) para um
+     * mapa interno. O arquivo deve conter linhas no formato "KEY=VALUE", e linhas
+     * vazias ou comentários (linhas que começam com "#") serão ignorados.
+     * 
+     * @param path O caminho relativo ao diretório do projeto para o arquivo .env a
+     *             ser carregado.
+     */
     public static void load(String path) {
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             br.lines()
@@ -24,6 +36,13 @@ public class EnvLoader {
         }
     }
 
+    /**
+     * Recupera o valor de uma variável de ambiente pelo nome da chave.
+     * 
+     * @param key O nome da variável de ambiente a ser recuperada.
+     * @return Retorna um Optional contendo o valor da variável, ou um Optional
+     *         vazio se a chave não existir.
+     */
     public static Optional<String> get(String key) {
         return Optional.ofNullable(env.get(key));
     }
